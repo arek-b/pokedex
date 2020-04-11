42d50e60fe42ff6cde5cc504974e0a721421160c
Arkadiusz Bergmann <arkadiuszbergmann@gmail.com>
2020-04-11 14:50:50 +0200

    collapsing navbar works

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "@reach/router";
 import * as Accents from "./Accents";
 
 function Navbar() {
+  const [collapse, setCollapse] = useState("");
+
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <Link to="/" className="navbar-brand">
@@ -11,16 +13,18 @@ function Navbar() {
       <button
         className="navbar-toggler"
         type="button"
-        data-toggle="collapse"
-        data-target="#navbarSupportedContent"
         aria-controls="navbarSupportedContent"
         aria-expanded="false"
         aria-label="Toggle navigation"
+        onClick={() => setCollapse(collapse == "" ? "show" : "")}
       >
         <span className="navbar-toggler-icon"></span>
       </button>
 
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <div
+        className={`collapse ${collapse} navbar-collapse`}
+        id="navbarSupportedContent"
+      >
         <ul className="navbar-nav mr-auto">
           <li className="nav-item active">
             <Link to="/" className="nav-link">
