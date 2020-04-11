ac4f691a09d3ee6d08dc3254c159b049cfe8dc00
Arkadiusz Bergmann <arkadiuszbergmann@gmail.com>
2020-04-11 11:15:58 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return <p>Welcome to Pokédex!</p>;
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
