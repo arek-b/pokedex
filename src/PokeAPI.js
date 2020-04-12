2a091f61a8e6093492fba7c4467f82407a97025e
Arkadiusz Bergmann <arkadiuszbergmann@gmail.com>
2020-04-12 10:37:54 +0200

    API: support for pagination & checking prop types

--- src/PokeAPI.js
+++ src/PokeAPI.js
@@ -1,9 +1,29 @@
 import "regenerator-runtime/runtime";
+import PropTypes from "prop-types";
 
 export default class PokeAPI {
   static async get(url) {
+    PropTypes.checkPropTypes(
+      {
+        url: PropTypes.string.isRequired,
+      },
+      { url }
+    );
     const response = await fetch(url);
     const json = await response.json();
     return json;
+  }
+
+  static async getPaginated(url, page, resultsPerPage) {
+    PropTypes.checkPropTypes(
+      {
+        url: PropTypes.string.isRequired,
+        page: PropTypes.number.isRequired,
+        resultsPerPage: PropTypes.number.isRequired,
+      },
+      { url, page, resultsPerPage }
+    );
+    url += `?limit=${resultsPerPage}&offset=${(page - 1) * resultsPerPage}`;
+    return await PokeAPI.get(url);
   }
 }
