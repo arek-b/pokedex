0ad731db29cd71f4060311a78724be0487760db9
Arkadiusz Bergmann <arkadiuszbergmann@gmail.com>
2020-04-12 10:38:24 +0200

    Pagination

---
+++ src/Pagination.js
@@ -0,0 +1,40 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export default function Pagination({ currentPage, pageCount, setPage }) {
+  let options = [];
+  let i = 1;
+  do {
+    options.push(
+      <option key={i} value={i}>
+        {i}
+      </option>
+    );
+    ++i;
+  } while (i <= pageCount);
+
+  return (
+    <div className="input-group">
+      <div className="input-group-prepend">
+        <span className="input-group-text">Page</span>
+      </div>
+      <select
+        className="form-control"
+        value={currentPage}
+        onChange={(e) => setPage(parseInt(e.target.value))}
+        onBlur={(e) => setPage(parseInt(e.target.value))}
+      >
+        {options}
+      </select>
+      <div className="input-group-append">
+        <span className="input-group-text">of {pageCount}</span>
+      </div>
+    </div>
+  );
+}
+
+Pagination.propTypes = {
+  currentPage: PropTypes.number,
+  pageCount: PropTypes.number,
+  setPage: PropTypes.func,
+};
--- src/PokemonListContainer.js
+++ src/PokemonListContainer.js
@@ -1,16 +1,38 @@
 import React, { useEffect, useState } from "react";
 import PokemonList from "./PokemonList";
 import PokeAPI from "./PokeAPI";
+import Pagination from "./Pagination";
 
 export default function PokemonListContainer() {
   const [pokemonList, setPokemonList] = useState([]);
+  const [page, setPage] = useState(1);
+  const [pageCount, setPageCount] = useState(1);
+  const resultsPerPage = 15;
 
   useEffect(() => {
-    // get pokemon list from api
-    PokeAPI.get("https://pokeapi.co/api/v2/pokemon/").then((apiResult) => {
+    setPokemonList([]);
+    PokeAPI.getPaginated(
+      "https://pokeapi.co/api/v2/pokemon/",
+      page,
+      resultsPerPage
+    ).then((apiResult) => {
       setPokemonList(apiResult.results);
+      setPageCount(Math.ceil(apiResult.count / resultsPerPage));
     });
-  }, []);
-
-  return <PokemonList pokemonList={pokemonList} />;
+  }, [page]);
+  return (
+    <div>
+      <div className="row">
+        <div className="col-4 col-sm-7 col-md-8 col-lg-9"></div>
+        <div className="col-8 col-sm-5 col-md-4 col-lg-3">
+          <Pagination
+            currentPage={page}
+            pageCount={pageCount}
+            setPage={setPage}
+          />
+        </div>
+      </div>
+      <PokemonList pokemonList={pokemonList} />
+    </div>
+  );
 }
