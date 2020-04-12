005757266d6286503d60d9bd05e4991ab724aaf9
Arkadiusz Bergmann <arkadiuszbergmann@gmail.com>
2020-04-12 11:24:25 +0200

    Pagination improved

--- src/Pagination.js
+++ src/Pagination.js
@@ -13,21 +13,33 @@ export default function Pagination({ currentPage, pageCount, setPage }) {
     ++i;
   } while (i <= pageCount);
 
+  let back = currentPage > 1;
+  let next = currentPage < pageCount;
+
   return (
-    <div className="input-group">
-      <div className="input-group-prepend">
-        <span className="input-group-text">Page</span>
-      </div>
-      <select
-        className="form-control"
-        value={currentPage}
-        onChange={(e) => setPage(parseInt(e.target.value))}
-        onBlur={(e) => setPage(parseInt(e.target.value))}
-      >
-        {options}
-      </select>
-      <div className="input-group-append">
-        <span className="input-group-text">of {pageCount}</span>
+    <div className="row">
+      <div className="col-12 my-pagination">
+        <button
+          className={`pagination-arrow ${back ? "" : "visibility-hidden"}`}
+          onClick={() => setPage(currentPage - 1)}
+        >
+          &lt;
+        </button>
+        <select
+          className="form-control"
+          value={currentPage}
+          onChange={(e) => setPage(parseInt(e.target.value))}
+          onBlur={(e) => setPage(parseInt(e.target.value))}
+        >
+          {options}
+        </select>
+        <button
+          className={`pagination-arrow ${next ? "" : "visibility-hidden"}`}
+          onClick={() => setPage(currentPage + 1)}
+        >
+          &gt;
+        </button>
+        <div>of&nbsp;{pageCount}</div>
       </div>
     </div>
   );
