fa35e045caca80d54da07afab166b0ce925937da
Arkadiusz Bergmann <arkadiuszbergmann@gmail.com>
2020-04-13 20:10:02 +0200

    removed search from navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -37,17 +37,6 @@ function Navbar() {
             </Link>
           </li>
         </ul>
-        <form className="form-inline my-2 my-lg-0">
-          <input
-            className="form-control mr-sm-2"
-            type="search"
-            placeholder="Search"
-            aria-label="Search"
-          />
-          <button className="btn btn-danger my-2 my-sm-0" type="submit">
-            Search
-          </button>
-        </form>
       </div>
     </nav>
   );
