6383d22d302c98d8169061e58934c68ee8d286d0
Arkadiusz Bergmann <arkadiuszbergmann@gmail.com>
2020-04-15 15:29:48 +0200

    fixed state update on unmounted component

--- src/PokemonListItemContainer.js
+++ src/PokemonListItemContainer.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import PokemonListItem from "./PokemonListItem";
 import PropTypes from "prop-types";
 import PokeAPI from "./PokeAPI";
@@ -7,13 +7,17 @@ import { navigate } from "@reach/router";
 export default function PokemonListItemContainer({ name, url }) {
   const [info, setInfo] = useState({});
   const [spriteLoaded, setSpriteLoaded] = useState(false);
+  const isMounted = useRef(true);
 
   useEffect(() => {
     PokeAPI.get(url).then((apiResult) => {
       if (apiResult) {
-        setInfo(apiResult);
+        if (isMounted.current) setInfo(apiResult);
       } else navigate("/api-connection-failed/");
     });
+    return () => {
+      isMounted.current = false;
+    };
   }, [url]);
 
   return (
