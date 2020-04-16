f68fc2cf16d0dec66922a64c7793a9f1b0ccd507
Arkadiusz Bergmann <arkadiuszbergmann@gmail.com>
2020-04-16 22:13:44 +0200

    improved PokemonScreen

--- src/PokemonScreen.js
+++ src/PokemonScreen.js
@@ -21,61 +21,57 @@ export default function PokemonScreen({ data }) {
               <PokemonBasicInfo info={data} />
             </div>
             <div className="col-12 col-md-6">
-              <div className="card">
-                <div className="card-body">
-                  <h5 className="card-title">Sprites</h5>
-                  {Object.values(data.sprites).every(
-                    (value) => value === null // if every value is null
-                  ) ? (
-                    <p className="text-muted">
-                      No sprites available for this {Accents.Pokemon}
-                    </p>
-                  ) : (
-                    <div className="row sprites-row">
-                      <SpriteCard
-                        src={data.sprites.front_default}
-                        alt="Front sprite"
-                        description="Front"
-                      />
-                      <SpriteCard
-                        src={data.sprites.back_default}
-                        alt="Back sprite"
-                        description="Back"
-                      />
-                      <SpriteCard
-                        src={data.sprites.front_female}
-                        alt="Front female sprite"
-                        description="Female"
-                      />
-                      <SpriteCard
-                        src={data.sprites.back_female}
-                        alt="Back female sprite"
-                        description="Female back"
-                      />
-                      <SpriteCard
-                        src={data.sprites.front_shiny}
-                        alt="Front sprite (shiny)"
-                        description="Front (shiny)"
-                      />
-                      <SpriteCard
-                        src={data.sprites.back_shiny}
-                        alt="Back sprite (shiny)"
-                        description="Back (shiny)"
-                      />
-                      <SpriteCard
-                        src={data.sprites.front_shiny_female}
-                        alt="Front female sprite (shiny)"
-                        description="Female (shiny)"
-                      />
-                      <SpriteCard
-                        src={data.sprites.back_shiny_female}
-                        alt="Back sprite (shiny)"
-                        description="Female back (shiny)"
-                      />
-                    </div>
-                  )}
+              <h5 className="card-title border-bottom pb-2">Sprites</h5>
+              {Object.values(data.sprites).every(
+                (value) => value === null // if every value is null
+              ) ? (
+                <p className="text-muted">
+                  No sprites available for this {Accents.Pokemon}
+                </p>
+              ) : (
+                <div className="row sprites-row">
+                  <SpriteCard
+                    src={data.sprites.front_default}
+                    alt="Front sprite"
+                    description="Front"
+                  />
+                  <SpriteCard
+                    src={data.sprites.back_default}
+                    alt="Back sprite"
+                    description="Back"
+                  />
+                  <SpriteCard
+                    src={data.sprites.front_female}
+                    alt="Front female sprite"
+                    description="Female"
+                  />
+                  <SpriteCard
+                    src={data.sprites.back_female}
+                    alt="Back female sprite"
+                    description="Female back"
+                  />
+                  <SpriteCard
+                    src={data.sprites.front_shiny}
+                    alt="Front sprite (shiny)"
+                    description="Front (shiny)"
+                  />
+                  <SpriteCard
+                    src={data.sprites.back_shiny}
+                    alt="Back sprite (shiny)"
+                    description="Back (shiny)"
+                  />
+                  <SpriteCard
+                    src={data.sprites.front_shiny_female}
+                    alt="Front female sprite (shiny)"
+                    description="Female (shiny)"
+                  />
+                  <SpriteCard
+                    src={data.sprites.back_shiny_female}
+                    alt="Back sprite (shiny)"
+                    description="Female back (shiny)"
+                  />
                 </div>
-              </div>
+              )}
             </div>
           </div>
         </div>
